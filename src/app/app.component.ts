39486ad2ff55659d19b36af9b54829d08f39e1a3
jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-02-20 16:17:00 -0500

    0220

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'imooc-pinduoduo';
-  tabs = ['热门', '男装', '手机']
+  
 }
+ 
